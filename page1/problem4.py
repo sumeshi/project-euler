3fd75167088f702926896f4371586b853cc545b1
S.Nakano <j15322sn@edu.tuis.ac.jp>
2018-05-11 06:03:55 +0900

    [fix] brushup p4

--- page1/problem4.py
+++ page1/problem4.py
@@ -5,13 +5,12 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 def palindrome():
-    l = m = []
-    for i in range(100, 1000):
-        for j in range(100, 1000):
+    m = []
+    for i in range(100, 1000)[::-1]:
+        for j in range(100, i)[::-1]:
             num = i*j
             if str(num)[::-1] == str(num):
-                l.append(num)
-        if len(l)>0: m.append(max(l))
+                m.append(num)
     return max(m)
-                
+
 print(palindrome())
