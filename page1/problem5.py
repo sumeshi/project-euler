d94205ddd43cdd4676bb5c5b6275c863c46b69b5
S.Nakano <j15322sn@edu.tuis.ac.jp>
2018-05-11 06:52:23 +0900

    [fix] brushup p5

--- page1/problem5.py
+++ page1/problem5.py
@@ -4,17 +4,16 @@ problem5
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
-
 import problem3
 
-def min_multiple():
-    processing_num = range(1,21)[::-1]
-    n = 20
+import math
+from functools import reduce
 
-    while(True):
-        for i in processing_num:
-            if n%i != 0: break
-            if i == 1: return n*i
-        n+=1
+def lcm(x, y):
+    return (x * y) // math.gcd(x, y)
 
-print(min_multiple())
+def m_lcm(*args):
+    return reduce(lcm, args)
+
+processing_num = list(filter(lambda i: problem3.is_prime(i), range(1,21)))
+print(m_lcm(*processing_num))
